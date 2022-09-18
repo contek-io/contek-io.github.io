20c32f51dea9d55045a872d954bb9de315ae2012
dreamerryao <zuobinwang3@gmail.com>
2022-09-18 22:11:15 +0800

    feat(desktop): add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,56 @@
+import { ReactComponent as Logo } from '@/assets/logo.svg';
+import { ALL_RIGHTS_RESERVED, FOOTER_LINKS } from '@/constants';
+import { DESC_CLASS } from '@/styles';
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className='md:pb-28 pb-12'>
+      <div className='flex w-full px-6 xl:px-60 lg:px-40 md:px-20 justify-between'>
+        {/* logo */}
+        <div className='hidden md:block 2xl:mr-80 xl:mr-52 lg:mr-40 md:mr-16'>
+          <span className='flex font-bold md:text-2xl text-sm items-center mb-24'>
+            <Logo className='md:mr-2 mr-1' />
+            CONTEK
+          </span>
+          <span className='whitespace-pre text-sm'>{ALL_RIGHTS_RESERVED}</span>
+        </div>
+        <div className='md:grid md:grid-cols-3 gap-0 md:gap-10 2xl:gap-32 xl:gap-20 lg:gap-12 w-full md:w-auto flex justify-between'>
+          {FOOTER_LINKS.map((links, index) => {
+            return (
+              <p key={`links-${index}`}>
+                <div className={`${DESC_CLASS} font-bold mb-4 md:mb-10`}>
+                  {links.title}
+                </div>
+                {links.details.map((link) => {
+                  return (
+                    <>
+                      {link.name && (
+                        <div
+                          className={`xl:text-lg lg:text-base md:text-sm text-xs  mb-2 md:mb-4`}
+                        >
+                          {link.name}
+                        </div>
+                      )}
+                      {link.icon ? (
+                        <div className={`${DESC_CLASS}  mb-2 md:mb-4`}>
+                          {link.icon}
+                        </div>
+                      ) : null}
+                    </>
+                  );
+                })}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+      <div className='md:hidden block w-full px-6 xl:px-60 lg:px-40 md:px-20 mt-8'>
+        <span className='flex font-bold md:text-2xl text-sm items-center mb-2'>
+          <Logo className='md:mr-2 mr-1' />
+          CONTEK
+        </span>
+        <span className='text-sm'>{ALL_RIGHTS_RESERVED}</span>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,6 @@
+import { ReactNode } from 'react';
+
+import { ReactComponent as LinkedIn } from '@/assets/linkedin.svg';
 import memberPng from '@/assets/member_example.png';
 interface Member {
   img: string;
@@ -6,6 +9,16 @@ interface Member {
 interface Feature {
   title: string;
   desc: string;
+}
+
+interface Link {
+  name?: string;
+  icon?: ReactNode;
+  onClick?: () => void;
+}
+interface FooterLink {
+  title: string;
+  details: Link[];
 }
 
 const ABOUT_TITLE = 'What We Do';
@@ -17,6 +30,10 @@ const VALUE_DESC =
   'Our Values provide the foundation upon which our business is built. They support our vision, shape our culture and reflect the personality of our people. They are the behavioural traits that are inherent in our business - the traits that are special to us, critical to our success and must be kept at all costs. We created these values to describe what was important to us, but you can also look at them through another lens and see them as opposites of the things we try to avoid.';
 const JOIN_US_TITLE = 'Join Us';
 const JOIN_US_DESC = 'Visit our careers page to see current job openings';
+
+const ALL_RIGHTS_RESERVED = `© ${new Date().getFullYear()} Contek trading, LLC.
+All rights reserved.`;
+
 const MEMBER_LIST: Member[] = [
   { img: memberPng, name: 'Cauliflower J.' },
   { img: memberPng, name: 'Cauliflower J.Cauliflower J.Cauliflower J.' },
@@ -47,10 +64,52 @@ const FEATURES: Feature[] = [
     desc: 'The key to success isn’t just the work itself, it’s the way in which people collaborate in order to do the work. ',
   },
 ];
+
+const FOOTER_LINKS: FooterLink[] = [
+  {
+    title: 'Contact Us',
+    details: [
+      {
+        name: 'info@contek.io',
+      },
+      {
+        icon: <LinkedIn />,
+      },
+    ],
+  },
+  {
+    title: 'Company',
+    details: [
+      {
+        name: 'Careers',
+      },
+      {
+        name: 'Team',
+      },
+    ],
+  },
+  {
+    title: 'Locations',
+    details: [
+      {
+        name: 'Shenzhen',
+      },
+      {
+        name: 'Hong Kong',
+      },
+      {
+        name: 'Singapore',
+      },
+    ],
+  },
+];
+
 export {
   ABOUT_DESC,
   ABOUT_TITLE,
+  ALL_RIGHTS_RESERVED,
   FEATURES,
+  FOOTER_LINKS,
   JOIN_US_DESC,
   JOIN_US_TITLE,
   MEMBER_LIST,
